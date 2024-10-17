f0533df8d8641e09820f1169954536bed325d5ac
JuliusDgenius <ibejulius1@gmail.com>
2024-10-17 18:31:39 +0100

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,9 +5,8 @@
 		-- name, string (255 characters)
 	-- If the table already exists, your script should not fail
 	-- Your script can be executed on any database
--- Context: Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
 CREATE TABLE users IF NOT EXISTS (
-	id INT NOT NULL AUTOINCREMENT PIMARY KEY (id),
+	id INT NOT NULL AUTO_INCREMENT PIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-	);
+)
