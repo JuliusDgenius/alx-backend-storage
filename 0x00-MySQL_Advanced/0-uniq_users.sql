5aae913ceb147864379a4b2d474dc62842f2a627
JuliusDgenius <ibejulius1@gmail.com>
2024-10-17 18:37:44 +0100

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,8 +5,8 @@
 		-- name, string (255 characters)
 	-- If the table already exists, your script should not fail
 	-- Your script can be executed on any database
-CREATE TABLE users IF NOT EXISTS (
-	id int NOT NULL AUTO_INCREMENT PIMARY KEY,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 )
