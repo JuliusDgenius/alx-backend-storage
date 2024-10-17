a93b244647c1ceb25b465d42ed8339eefc10142b
JuliusDgenius <ibejulius1@gmail.com>
2024-10-17 18:51:18 +0100

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Write a script that creates a table users with the following
+-- fields id, email, name,
+-- country, enumeration of countries: US, CO and TN,
+-- never null (= default will be the first element of the enumeration, here US)
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	country ENUM('US', 'CO', 'INT') NOT NULL DEFAULT 'US'
+);
