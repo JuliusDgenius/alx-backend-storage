1814f0f0a0dc6efc2eff323000eeb767262d1ddb
JuliusDgenius <ibejulius1@gmail.com>
2024-10-17 18:18:00 +0100

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,13 @@
+-- Write a SQL script that creates a table users following these requirements:
+	-- With these attributes:
+		-- id, integer, never null, auto increment and primary key
+		-- email, string (255 characters), never null and unique
+		-- name, string (255 characters)
+	-- If the table already exists, your script should not fail
+	-- Your script can be executed on any database
+-- Context: Make an attribute unique directly in the table schema will enforced your business rules and avoid bugs in your application
+CREATE TABLE users IF NOT EXISTS (
+	id INT NOT NULL AUTOINCREMENT PIMARY KEY (id),
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	);
